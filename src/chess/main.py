31d448d41910b0c0a56866c2209ebe9da6a5cf8e
Rob Pitkin <43574523+rob-pitkin@users.noreply.github.com>
2025-11-05 10:09:58 -0800

    Fix training the chess model

--- src/chess/main.py
+++ src/chess/main.py
@@ -1,10 +1,14 @@
 from pettingzoo.classic import chess_v6
 from src.lib.ppo import train_action_mask, eval_action_mask
+from torch.distributions import Distribution
 
 if __name__ == "__main__":
     env_fn = chess_v6
 
     env_kwargs = {}
+
+    # Disable distribution validation due to known issue: https://github.com/Stable-Baselines-Team/stable-baselines3-contrib/issues/247
+    Distribution.set_default_validate_args(False)
 
     # Train a model against itself
     train_action_mask(env_fn, steps=20_480, seed=0, **env_kwargs)
